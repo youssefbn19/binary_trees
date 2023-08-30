57488d38887619ff18b943d209edf48e107632b3
Youssef Benchiir <ybenchiir@gmail.com>
2023-08-30 15:25:56 +0100

    fix betty checker error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - Inserts a node as the right-child of another node.
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
  *
  * @parent: A pointer to the node to insert the right-child in.
  * @value: The value to store in the new node.
